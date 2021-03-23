7a0d2352377432874f4a789047e58e8b9288aabe
catSIXe <github2.konrad@sch.umann.it>
2021-03-23 19:21:05 +0000

    splitting up the LoRaWAN Connector into normal and bridge(special converter hardware) mode

--- types/connectors/LoRaWANConnector.js
+++ types/connectors/LoRaWANConnector.js
@@ -1,6 +1,8 @@
 const Connector = require("./Connector")
 
 class LoRaWANConnector extends Connector {
+  // this is optimized for only receiving LoRa uplink Messages (from Birdy Pagers)
+
   constructor (amqpConnMngr) {
     super(amqpConnMngr)
     this.name = "lorawan"
