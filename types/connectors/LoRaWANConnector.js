528ce0aa74966d6791bec17ea59aae1d82d265d4
catSIXe <github2.konrad@sch.umann.it>
2021-04-02 10:03:39 +0000

    added TTN v2 MQTT Support

--- types/connectors/LoRaWANConnector.js
+++ types/connectors/LoRaWANConnector.js
@@ -19,18 +19,28 @@ class LoRaWANConnector extends Connector {
   }
   async onMQTTConnect() {
     await this.client.subscribe(`v3/${ config.connectors.lorawan.username }/devices/#`)
+    await this.client.subscribe(`${ config.connectors.lorawan.username }/devices/#`)
     console.log('[lorawan] subscribed')
   }
   async onMQTTMessage(topic, message) {
     //if (topic.indexOf('/up') > -1) return
     const json = JSON.parse(Buffer.from(message).toString('utf-8'))
     console.log(topic, json)
-    if (!!json.uplink_message) {
+    if (!!json.uplink_message) { // TTN v3
       this.connectorRegistry.events.emit('response', {
         ...json.uplink_message.decoded_payload,
         port: json.uplink_message.f_port,
         date: new Date(json.received_at),
         device_id: json.end_device_ids.device_id,
+        metadata: json,
+      })
+    }
+    if (!!json.app_id && json.app_id == config.connectors.lorawan.username) { // TTN v2 
+      this.connectorRegistry.events.emit('response', {
+        ...json.payload_fields,
+        port: json.port,
+        date: new Date(json.metadata.time),
+        device_id: json.dev_id,
         metadata: json,
       })
     }
